a77c91c7852a4295963bae54ad03f29e0c65a4a8
☆*:.｡.o(≧▽≦)o.｡.:*☆ <67199852+surger-y@users.noreply.github.com>
2024-03-07 16:54:17 -0500

    Added trailibng color effect and separate scrolling

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,32 @@ for (let i = 0; i < vanish.length; i++) {
     /([\w'-]+|[^\w\s])/g,
     "<span>$1</span>"
   );
+  function generateRandomInteger(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+  }
+  
+  // After setting innerHTML, select the newly created spans
+  let spans = vanish[i].querySelectorAll("span");
+
+  // Apply a unique color to each span
+  spans.forEach(span => {
+    const r = generateRandomInteger(0, 255);
+    const g = generateRandomInteger(0, 255);
+    const b = generateRandomInteger(0, 255);
+    span.style.backgroundColor = `rgb(${r},${g},${b})`;
+  });
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
