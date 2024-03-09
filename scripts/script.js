d2a6be50a5753440919671a05f93d519d2ab5cb9
☆*:.｡.o(≧▽≦)o.｡.:*☆ <67199852+surger-y@users.noreply.github.com>
2024-03-08 20:50:24 -0500

    I added a breif load in part to the site, a moment to take a deep breath. I also moved the nav to outside of the main two collumn stucture, which makes sense since it floats above

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,37 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  var overlay = document.getElementById('overlay');
+  
+  // Set a transitionend event listener
+  overlay.addEventListener("transitionend", function(event) {
+    // Check if the opacity transition has ended
+    if (event.propertyName === 'opacity') {
+      // Remove the id attribute from the overlay
+      overlay.removeAttribute('id');
+    }
+  });
+  
+  // Trigger the fade-out effect by changing the opacity
+  overlay.style.opacity = 0;
+});
+
+// WRAPS EACH WORD IN "VANISH" CLASS ELEMENTS IN SPANS
 let vanish = document.querySelectorAll(".vanish");
 for (let i = 0; i < vanish.length; i++) {
   vanish[i].innerHTML = vanish[i].textContent.replace(
     /([\w'-]+|[^\w\s])/g,
     "<span>$1</span>"
   );
+
+
+  // CREATES AND APPLIES RANDOM RGB VALUES TO "VANISH" SPAN BACKGROUNDS
   function generateRandomInteger(min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
   }
-  
-  // After setting innerHTML, select the newly created spans
+
   let spans = vanish[i].querySelectorAll("span");
 
-  // Apply a unique color to each span
   spans.forEach(span => {
     const r = generateRandomInteger(0, 255);
     const g = generateRandomInteger(0, 255);
@@ -21,6 +40,7 @@ for (let i = 0; i < vanish.length; i++) {
   });
 
 }
+
 
 
 
