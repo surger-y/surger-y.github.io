f4757565c5f94d3b4f5c132189b060887b76e29d
surger-y <offline.surgery@gmail.com>
2024-05-03 16:55:02 -0400

    Now serving html via fetch api to update left and right sides of site

--- blog-posts/digital-life.md
+++
@@ -1,9 +0,0 @@
-This is a test to see if I can serve markdown to my webpage without reloading.
-**TEST**
-# Test
-
-* test1
-* test2
-* test3
-
-
---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+const image = document.querySelector('img')
+const backRight = document.querySelector('.backRight')
+const leftSide = document.querySelector('.left-wrapper')
+const rightSide = document.querySelector('.right-wrapper')
+
+const loadMainLeft = () => {
+  fetch('https://raw.githubusercontent.com/surger-y/surger-y.github.io/main/pages/main-left.html')
+    .then(response => response.text())
+    .then(result => leftSide.innerHtml = result)
+}
+
+const loadMainRight = () => {
+  fetch('https://raw.githubusercontent.com/surger-y/surger-y.github.io/main/pages/main-right.html')
+    .then(response => response.text())
+    .then(result => rightSide.innerHtml = result)
+}
+
+loadMainLeft();
+loadMainRight();
+
+backRight.addEventListener('mousedown', () => {
+  loadMainRight()
+}
+
+image.addEventListener('mousedown', () => {
+  fetch('https://raw.githubusercontent.com/surger-y/surger-y.github.io/main/pages/blog/blog001.html')   
+   .then(response => response.text())
+   .then(result => rightSide.innerText = result);
+
+})
+
+
