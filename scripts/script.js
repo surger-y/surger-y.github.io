78c21227cf9f0565eb5c6a4b3be3f621aa8387ee
surger-y <offline.surgery@gmail.com>
2024-05-03 16:59:27 -0400

    check

--- scripts/script.js
+++ scripts/script.js
@@ -18,9 +18,9 @@ const loadMainRight = () => {
 loadMainLeft();
 loadMainRight();
 
-backRight.addEventListener('mousedown', () => {
-  loadMainRight()
-})
+//backRight.addEventListener('mousedown', () => {
+ // loadMainRight()
+//})
 
 image.addEventListener('mousedown', () => {
   fetch('https://raw.githubusercontent.com/surger-y/surger-y.github.io/main/pages/blog/blog001.html')   
